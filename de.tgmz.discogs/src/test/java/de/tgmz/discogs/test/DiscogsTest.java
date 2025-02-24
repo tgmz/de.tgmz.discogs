e57c82c6fea9807274c04c9dd20001dffa0c1a2c
Thomas Zierer <thomas.zierer@muenchen-mail.de>
2025-02-24 09:22:33 +0100

    Introduce filteredTracklist method

--- de.tgmz.discogs/src/test/java/de/tgmz/discogs/test/DiscogsTest.java
+++ de.tgmz.discogs/src/test/java/de/tgmz/discogs/test/DiscogsTest.java
@@ -91,14 +91,20 @@ public class DiscogsTest {
 	public void testSubtrack() {
 		Release r = em.find(Release.class, 2460568L);
 		assertTrue(r.getTracklist().get(10).getSubTracklist().isEmpty());
-		assertEquals("Sometimes I Feel Like A Motherless Child", r.getTracklist().get(11).getSubTracklist().get(0).getTitle());
+		assertEquals("Sometimes I Feel Like A Motherless Child", r.getFilteredTracklist().get(11).getSubTracklist().get(0).getTitle());
 		assertEquals(12, r.getTracklist().get(11).getTrackNumber());
+		assertEquals(12, r.getFilteredTracklist().get(11).getTrackNumber());
 	}
 	@Test
 	public void testEmptyTrack() {
 		Release r = em.find(Release.class, 20279608L);
-		assertEquals(22, r.getTracklist().size());
-		assertEquals(22, r.getTracklist().get(21).getTrackNumber());
+		assertEquals(25, r.getTracklist().size());
+		
+		List<Track> ftl = r.getFilteredTracklist();
+		
+		assertEquals(22, ftl.size());
+		assertEquals(1, ftl.get(0).getTrackNumber());
+		assertEquals(22, ftl.get(21).getTrackNumber());
 	}
 	private static void load() throws IOException,SAXException {
 		try (InputStream is = new FileInputStream(DiscogsFile.ARTISTS.getFile())) {
